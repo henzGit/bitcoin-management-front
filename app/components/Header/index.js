3b2a6203aeefd6a7dfc5096837140280535785f9
henz <henz.jb@gmail.com>
2018-10-05 20:52:13 +0900

    correct header components

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -15,35 +15,12 @@ class Header extends React.Component {
           url: '/',
         },
         {
-          label: 'File',
-          icon: 'pi pi-fw pi-file',
-          items: [
-            {
-              label: 'New',
-              icon: 'pi pi-fw pi-plus',
-              items: [
-                {
-                  label: 'Bookmark',
-                  icon: 'pi pi-fw pi-bookmark',
-                },
-                {
-                  label: 'Video',
-                  icon: 'pi pi-fw pi-video',
-                },
-              ],
-            },
-            {
-              label: 'Delete',
-              icon: 'pi pi-fw pi-trash',
-            },
-            {
-              separator: true,
-            },
-            {
-              label: 'Export',
-              icon: 'pi pi-fw pi-external-link',
-            },
-          ],
+          label: 'Coin',
+          icon: 'pi pi-fw pi-star',
+        },
+        {
+          label: 'Profile',
+          icon: 'pi pi-fw pi-user',
         },
       ],
     };
