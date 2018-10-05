5b48b7aafbe5c9b23da39fff3735cd167cdf3b82
henz <henz.jb@gmail.com>
2018-10-05 20:46:54 +0900

    remove unused components on header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -12,7 +12,7 @@ class Header extends React.Component {
         {
           label: 'Home',
           icon: 'pi pi-fw pi-home',
-          url: '/'
+          url: '/',
         },
         {
           label: 'File',
@@ -42,92 +42,6 @@ class Header extends React.Component {
             {
               label: 'Export',
               icon: 'pi pi-fw pi-external-link',
-            },
-          ],
-        },
-        {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-            {
-              label: 'Left',
-              icon: 'pi pi-fw pi-align-left',
-            },
-            {
-              label: 'Right',
-              icon: 'pi pi-fw pi-align-right',
-            },
-            {
-              label: 'Center',
-              icon: 'pi pi-fw pi-align-center',
-            },
-            {
-              label: 'Justify',
-              icon: 'pi pi-fw pi-align-justify',
-            },
-          ],
-        },
-        {
-          label: 'Users',
-          icon: 'pi pi-fw pi-user',
-          items: [
-            {
-              label: 'New',
-              icon: 'pi pi-fw pi-user-plus',
-            },
-            {
-              label: 'Delete',
-              icon: 'pi pi-fw pi-user-minus',
-            },
-            {
-              label: 'Search',
-              icon: 'pi pi-fw pi-users',
-              items: [
-                {
-                  label: 'Filter',
-                  icon: 'pi pi-fw pi-filter',
-                  items: [
-                    {
-                      label: 'Print',
-                      icon: 'pi pi-fw pi-print',
-                    },
-                  ],
-                },
-                {
-                  icon: 'pi pi-fw pi-bars',
-                  label: 'List',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          label: 'Events',
-          icon: 'pi pi-fw pi-calendar',
-          items: [
-            {
-              label: 'Edit',
-              icon: 'pi pi-fw pi-pencil',
-              items: [
-                {
-                  label: 'Save',
-                  icon: 'pi pi-fw pi-calendar-plus',
-                },
-                {
-                  label: 'Delete',
-                  icon: 'pi pi-fw pi-calendar-minus',
-                },
-              ],
-            },
-            {
-              label: 'Archieve',
-              icon: 'pi pi-fw pi-calendar-times',
-              items: [
-                {
-                  label: 'Remove',
-                  icon: 'pi pi-fw pi-calendar-minus',
-                },
-              ],
             },
           ],
         },
