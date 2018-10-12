41d714674ce49ffdfb8e19a00ba0a2c09ee5b1b6
henz <henz.jb@gmail.com>
2018-10-12 20:45:25 +0900

    add constants for AuthPage

---
+++ app/containers/AuthPage/constants.js
@@ -0,0 +1,9 @@
+/*
+ *
+ * AuthPage constants
+ *
+ */
+
+export const AUTHENTICATE = 'app/AuthPage/AUTHENTICATE';
+export const CHANGE_USERNAME = 'app/AuthPage/CHANGE_USERNAME';
+export const CHANGE_PASSWORD = 'app/AuthPage/CHANGE_PASSWORD';
