3c45752234226f2d15a14f106990ea1d88cd1b4e
henz <henz.jb@gmail.com>
2018-10-13 13:24:12 +0900

    add authenticate success and error constants

--- app/containers/AuthPage/constants.js
+++ app/containers/AuthPage/constants.js
@@ -4,6 +4,8 @@
  *
  */
 
-export const AUTHENTICATE = 'app/AuthPage/AUTHENTICATE';
 export const CHANGE_USERNAME = 'app/AuthPage/CHANGE_USERNAME';
 export const CHANGE_PASSWORD = 'app/AuthPage/CHANGE_PASSWORD';
+export const AUTHENTICATE = 'app/AuthPage/AUTHENTICATE';
+export const AUTHENTICATE_SUCCESS = 'app/AuthPage/AUTHENTICATE_SUCCESS';
+export const AUTHENTICATE_ERROR = 'app/AuthPage/AUTHENTICATE_ERROR';
