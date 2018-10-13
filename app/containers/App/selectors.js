298279e6ef46c53232b709aad7cba84ffe901553
henz <henz.jb@gmail.com>
2018-10-13 19:15:54 +0900

    add selectors for state with global scope

--- app/containers/App/selectors.js
+++ app/containers/App/selectors.js
@@ -6,6 +6,22 @@ import { createSelector } from 'reselect';
 
 const selectGlobal = state => state.get('global');
 
+const makeSelectUserId = () =>
+  createSelector(selectGlobal, globalState => globalState.get('userId'));
+
+const makeSelectCookie = () =>
+  createSelector(selectGlobal, globalState => globalState.get('cookie'));
+
+const makeSelectIsAuthenticated = () =>
+  createSelector(selectGlobal, globalState => globalState.get('isAuthenticated'));
+
+const makeSelectGlobal = () =>
+  createSelector(selectGlobal, globalState => globalState);
+
 export {
   selectGlobal,
+  makeSelectGlobal,
+  makeSelectCookie,
+  makeSelectIsAuthenticated,
+  makeSelectUserId
 };
