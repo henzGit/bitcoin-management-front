cfc59685722128566aca10a8e0dd8232a5746ae8
henz <henz.jb@gmail.com>
2018-10-14 17:04:52 +0900

    update HomePage selectors

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -7,7 +7,6 @@ import { initialState } from './reducer';
 
 const selectHome = state => state.get('home', initialState);
 
-const makeSelectUsername = () =>
-  createSelector(selectHome, homeState => homeState.get('username'));
-
-export { selectHome, makeSelectUsername };
+export {
+  selectHome
+};
