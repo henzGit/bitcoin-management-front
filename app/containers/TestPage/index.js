7450ffe9e3adb5682c8924a18589552dbc35fa58
henz <henz.jb@gmail.com>
2018-10-14 20:54:26 +0900

    correct comment

--- app/containers/TestPage/index.js
+++ app/containers/TestPage/index.js
@@ -1,15 +1,12 @@
 /*
- * FeaturePage
+ * TestPage
  *
- * List all the features
+ * Testing purpose only
  */
 import React from 'react';
 import { RadioButton } from 'primereact/radiobutton';
 
 export default class TestPage extends React.Component {
-  // eslint-disable-line react/prefer-stateless-function
-  // Since state and props are static,
-  // there's no need to re-render this component
   constructor() {
     super();
     this.state = {
