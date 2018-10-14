36a79af2545370ffd8d16db47664284c576d93c7
henz <henz.jb@gmail.com>
2018-10-14 16:41:08 +0900

    update HomePage constant

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -8,5 +8,5 @@
  * Follow this format:
  * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
  */
+export const LOAD_USER_DATA = 'app/HomePage/LOAD_USER_DATA';
 
-export const CHANGE_USERNAME = 'app/HomePage/CHANGE_USERNAME';
