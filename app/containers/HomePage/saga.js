4ccdf063e7c80f893599403cf5188c9dd3c87b1b
henz <henz.jb@gmail.com>
2018-10-14 18:23:26 +0900

    update HomePaga saga

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -1,38 +1,57 @@
 /**
- * Gets the repositories of the user from Github
+ * Load user data for Home Page
  */
 
 import { call, put, select, takeLatest } from 'redux-saga/effects';
-import { LOAD_REPOS } from 'containers/App/constants';
-import { reposLoaded, repoLoadingError } from 'containers/App/actions';
+import {LOAD_USER_DATA} from './constants';
+import {
+  loadUserDataSuccess,
+  loadUserDataError,
+} from './actions';
 
 import request from 'utils/request';
-import { makeSelectUsername } from 'containers/HomePage/selectors';
+import { updateUserData}  from '../App/actions';
+import { Cookies } from 'react-cookie';
+import { push } from 'react-router-redux';
 
 /**
- * Github repos request/response handler
+ * Auth API request/response handler
  */
-export function* getRepos() {
-  // Select username from store
-  const username = yield select(makeSelectUsername());
-  const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
+export function* callApiGetUserDataForHomePage() {
+
+  const cookies = new Cookies();
+  const cookie = cookies.get('siteCookie');
+
+  // redirect to Auth Page
+  if (typeof cookie === "undefined" ||
+    typeof cookie.userId === "undefined" ||
+    typeof cookie.authId === "undefined") {
+      yield put(push('/auth'));
+      return;
+  }
+
+  const requestURL =
+    `https://api.github.com/users/henzGit/repos?type=all&sort=updated`;
 
   try {
     // Call our request helper (see 'utils/request')
-    const repos = yield call(request, requestURL);
-    yield put(reposLoaded(repos, username));
+    const response = yield call(request, requestURL);
+    const userData = {
+      isAuthenticated: true,
+      userId: 1,
+      currentUserName: 'great Henz'
+    };
+
+    yield put(loadUserDataSuccess());
+    yield put(updateUserData(userData));
   } catch (err) {
-    yield put(repoLoadingError(err));
+    yield put(loadUserDataError(err));
   }
 }
 
 /**
  * Root saga manages watcher lifecycle
  */
-export default function* githubData() {
-  // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
-  // By using `takeLatest` only the result of the latest API call is applied.
-  // It returns task descriptor (just like fork) so we can continue execution
-  // It will be cancelled automatically on component unmount
-  yield takeLatest(LOAD_REPOS, getRepos);
+export default function* loadUserData() {
+  yield takeLatest(LOAD_USER_DATA, callApiGetUserDataForHomePage );
 }
